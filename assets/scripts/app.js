acbfbcd1c55398940d21cb15e9b36b187d99e79c
MrFirel <william9516.wms@gmail.com>
2022-08-17 16:31:55 -0300

    Log update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -150,8 +150,13 @@ function endRound() {
 }
 
 function printLogHandler() {
+    let i = 0;
     for (const logEntry of battleLog) {
-        console.log(logEntry);
+        console.log(`#${i}`);
+        for (const key in logEntry) {
+            console.log(key + ' = ' + logEntry[key]);      
+        }
+        i++;
     }
 }
 
